c5b011c484b28059b5f534ae336e5cc0bfecffe7
SGVF\nguyen.anh-tai1 <nguyen.anh-tai1@hdsaison.com.vn>
2023-12-01 16:34:09 +0700

    update timetable table

--- source/src/main/java/com/project/model/entity/Timetable.java
+++ source/src/main/java/com/project/model/entity/Timetable.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Date;
 
 @Entity
 @Table(name = "timetable")
@@ -18,11 +19,17 @@ public class Timetable {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "course_id")
-    private Long courseId;
-
     @Column(name = "teacher_id")
     private Long teacherId;
+
+    @Column(name = "classroom_name")
+    private String classroomName;
+
+    @Column(name = "shift")
+    private String shift;
+
+    @Column(name = "implement_date")
+    private Date implementDate;
 
     @Column(name = "created_date", updatable = false)
     @CreationTimestamp
